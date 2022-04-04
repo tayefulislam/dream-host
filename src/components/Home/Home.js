ace9163513734a64b5f8a054603964832dd5092c
tayeful912 <kazitayefulislam12@gmail.com>
2022-04-04 12:53:12 +0600

    title and domain section complete of home route/page (only for computer)

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
+
+
     return (
         <div>
 
-            this is home
+            <div className="home-container">
+
+                <div className="title-container">
+
+                    <h1>Register Domain Name here!</h1>
+                    <h2>Engage customers / users with the best domain..</h2>
+                    <p>
+                        Easy Setup With Instant Activation, DNS Management, No Hidden Cost</p>
+
+                    <button className='buy-btn'>Buy Domain</button>
+
+
+                </div>
+
+
+                <div className="domain-container">
+                    <img src="domain.jpg" alt="" />
+                </div>
+
+
+            </div>
+
+
+
+            <div className="reviews-container">
+
+            </div>
+
+
+
 
         </div>
     );
